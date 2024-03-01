c8486e945431180c850d82a0c82ac820ba578ee4
MomenHussein <98105343+MomenHussein@users.noreply.github.com>
2024-03-01 21:41:05 +0200

    Update Normal Equation.py

--- Normal Equation.py
+++ Normal Equation.py
@@ -1,41 +1,24 @@
-
+import random as random
 import numpy as np
+def Normal_Equation(features,labels):
+    X_transpose = np.transpose(features)
+    X_transpose_X = np.dot(X_transpose, features)
+    X_transpose_y = np.dot(X_transpose, labels)
+    y_hat = X_transpose_y
+    y = labels
+    base_price = random.random()
+    #price = random.random()
+    learning_rate = 0.1
+    r = random.random()*0.1  
+    b_hat = base_price + learning_rate*(y-y_hat)
+    w_hat = []
+    for i in range (len(features)-1):
+      w_hat.append((features[i] + learning_rate*r*(y-y_hat)))
+    sum = 0
+    for j in range (len(features)-1):
+      sum += (w_hat[i]*features[i])
+    W = sum + b_hat
+    return W
 
-def dotmultiplication(mat1=[[]],mat2=[[]]):
-    
-    dimensions1 = np.shape(mat1)
-    R1, C1 = dimensions1
-    dimensions2 = np.shape(mat2)
-    R2, C2 = dimensions2
-    if C1 == R2:
-        res = [[0 for x in range(R1)] for y in range(C2)] 
-     
-    # explicit for loops
-        for i in range(0, R1):
-            for j in range(0, C2):
-                for k in range(0, R2):
-                    res[i][j] += mat1[i][k] * mat2[k][j]
-        return res
-    else:
-        return "Sorry Invalid Dimensions"
-    
-        
-
-def inverse(a=[]):
-
-    return np.linalg.inv(a)
-
-def transpose(arr=[]):
-    return (list(map(list, zip(*arr))))
-    
-def NormalEquation(a=[],b=[]):
-    return (dotmultiplication(inverse(dotmultiplication(transpose(a),a))),dotmultiplication(transpose(a),b))
-
-# Another way of linear Equation
-def NormalEquation2(a=[],b=[]):
-    a_tran = transpose(a)
-    mult = dotmultiplication(a_tran,a)
-    mult_inv = inverse(mult)
-    mult2 = dotmultiplication(a_tran,b)
-    x_n = dotmultiplication(mult_inv,mult2)
-    return x_n
+W = Normal_Equation(features,labels)
+print(W)
